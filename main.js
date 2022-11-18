ea989bbdd6c758fd3c62a7b7d2b873b855293215
UnknownUser#09 <82215907+WebHeadAni@users.noreply.github.com>
2022-11-18 12:07:54 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -41,8 +41,6 @@ function draw() {
         objectDetector.detect(video, gotResult);
         for (i = 0; i < objects.length; i++) {
           document.getElementById("status").innerHTML = "Status : Object Detected";
-          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
- 
           fill(r,g,b);
           percent = floor(objects[i].confidence * 100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
